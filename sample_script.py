8abdeb6edd44757fec4cea93a56facda5725165b
Aaron <20aaron.orenstein@boiseschools.net>
2020-06-29 11:37:01 -0600

    Added matrix multiply function and test

--- sample_script.py
+++ sample_script.py
@@ -2,3 +2,25 @@ def merge_and_sort_lists(list1, list2):
 	final_list = list1 + list2
 	final_list.sort()
 	return final_list
+
+def matrix_multiply(matrixA, matrixB):
+    A, B = 0,1
+
+    num_rows = [len(matrix) for matrix in [matrixA, matrixB]]
+    if 0 in num_rows:
+        return []
+
+    num_cols = [len(matrix[0]) for matrix in [matrixA, matrixB]]
+    if 0 in num_cols or num_cols[A] != num_rows[B]:
+        return []
+
+    result = [[]] * num_rows[A]
+    for i in range(num_rows[A]):
+        result[i] = [0] * num_cols[B]
+
+    for row in range(num_rows[A]):
+        for col in range(num_cols[B]):
+            for i in range(num_cols[A]):
+                result[row][col] += matrixA[row][i] * matrixB[i][col]
+
+    return result
--- sample_script_tests.py
+++ sample_script_tests.py
@@ -1,15 +1,24 @@
 import unittest
-from sample_script import merge_and_sort_lists
+from sample_script import *
 
 class BasicFunctionTest(unittest.TestCase):
-	def test_merge_and_sort_basic_ints(self):
-		list_1_in = [3,2,1]
-		list_2_in = [10,11,5]
-		ret_val = merge_and_sort_lists(list_1_in, list_2_in)
-		expect = [1,2,3,5,10,11]
+    def test_merge_and_sort_basic_ints(self):
+        list_1_in = [3,2,1]
+        list_2_in = [10,11,5]
+        ret_val = merge_and_sort_lists(list_1_in, list_2_in)
+        expect = [1,2,3,5,10,11]
 
-		self.assertListEqual(expect, ret_val,
-		'Should merge and sort two numeric lists')	
+        self.assertListEqual(expect, ret_val,
+                'Should merge and sort two numeric lists')	
+
+    def test_matrix_multply(self):
+        matrixA = [[1,2,3],[4,5,6]]
+        matrixB = [[0,9],[8,7],[6,5]]
+        ret_val = matrix_multiply(matrixA, matrixB)
+        expect = [[34, 38],[76, 101]]
+
+        self.assertListEqual(expect, ret_val,
+                'Should multiply two matrices')
 
 
 if __name__ == '__main__':
